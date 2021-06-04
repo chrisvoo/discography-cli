06db002ce6bfecc260d13b7dc2d6d758e77d5dcf
Christian Castelli <voodoo81people@gmail.com>
2021-06-04 10:50:30 +0200

    Hello world, basic structure

---
+++ src/Example.tsx
@@ -0,0 +1,9 @@
+import { Text, Newline } from 'ink';
+
+export default () => (
+  <Text>
+    <Text color="green">Hello</Text>
+    <Newline />
+    <Text color="red">World</Text>
+  </Text>
+);
---
+++ src/bin.tsx
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { render } from 'ink';
+import Example from './Example';
+
+render(<Example />);
